9f9047aefb40d44bf57189d2a3bc5a8fd0c0cfec
Giovana Bragio <giovanacaroba@hotmail.com>
2023-08-23 17:36:51 -0300

    API de envio de email com Spring

---
+++ envio-email/ms-email/src/main/java/com/ms/email/dtos/EmailDto.java
@@ -0,0 +1,36 @@
+package com.ms.email.dtos;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class EmailDto {
+
+    @NotBlank
+    private String ownerRef;
+
+    @NotBlank
+    @Email
+    private String emailFrom;
+
+    @NotBlank
+    @Email
+    private String emailTo;
+
+    @NotBlank
+    private String subject;
+
+    @NotBlank
+    private String text;
+
+    @NotBlank
+    private String contact;
+
+    @NotBlank
+    private String cellphone;
+
+    @NotBlank
+    private String name;
+}
